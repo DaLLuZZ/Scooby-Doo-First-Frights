de8cc9e8d5dd1e1f9556d58ef2e043e5cd1d1203
DaLLuZZ <79600653+DaLLuZZ@users.noreply.github.com>
2022-07-23 20:35:36 +0300

    Create EntityManagerNetwork.c

---
+++ Source/EntityManagerNetwork.c
@@ -0,0 +1,21 @@
+Info on that file:
+0x650E90 in main menu
+
+=====================
+
+Error templates related to this file:
+
+Out of sync. Expected entity %d got %d.
+The spawned entity template type is
+%s
+The Last 5 entities created were:
+%d: %s
+%d: %s - deleted
+No Entity in this slot.
+..\..\Source\EntityManagerNetwork.c Couldn`t spawn entity '%s' from server
+..\..\Source\EntityManagerNetwork.c Out of sync. Expected entity %d got %d
+..\..\Source\EntityManagerNetwork.c Couldn`t spawn entity '%s' from server
+..\..\Source\EntityManagerNetwork.c Out of sync. Expected entity %d got %d
+..\..\Source\EntityManagerNetwork.c Sync error - could not find entity %d
+..\..\Source\EntityManagerNetwork.c Sync error - could not find entity %d
+..\..\Source\EntityManagerNetwork.c Sync error - could not find entity %d
