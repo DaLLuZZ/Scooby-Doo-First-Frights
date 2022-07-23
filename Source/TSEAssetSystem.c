c6a1c368fc386aa5646502f7abcc872e8ba5ae3e
DaLLuZZ <79600653+DaLLuZZ@users.noreply.github.com>
2022-07-23 20:47:05 +0300

    Create TSEAssetSystem.c

---
+++ Source/TSEAssetSystem.c
@@ -0,0 +1,14 @@
+Info on that file:
+0x64FF90 in main menu
+
+=====================
+
+Error templates related to this file:
+
+..\Source\TSEAssetSystem.c Asset system deletion queue full
+..\..\Source\TSEAssetSystem.c Asset system deletion queue full
+filesmaxassetcount
+alias
+hunkfile%s.hnk
+if_language
+..\..\Source\TSEAssetSystem.c GlobalAssetSetUnnamed
