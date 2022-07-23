e7ba6327f0bb87991139ffa1e0033c6a2e73859f
DaLLuZZ <79600653+DaLLuZZ@users.noreply.github.com>
2022-07-23 20:44:10 +0300

    Create TSEHeap.c

---
+++ Source/TSEHeap.c
@@ -0,0 +1,11 @@
+Info on that file:
+0x6500D0 in main menu
+
+=====================
+
+Error templates related to this file:
+
+Out of memory creating heap:
+..\..\Source\TSEHeap.c Failed to alloc %,u bytes from %s heap (%,u free)
+..\..\Source\TSEHeap.c Failed to alloc %,u (%,u) bytes from %s heap (%,u free)
+..\..\Source\TSEHeap.c
